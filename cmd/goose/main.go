SteveOUasd/goose

commit 805f9b2105b7237c17dff3f00be739ddb9730cb6

allow to specify migrations table

cmd/goose/main.go
@@ -11,7 +11,8 @@ import (
 
 var (
 	flags   = flag.NewFlagSet("goose", flag.ExitOnError)
-	dir     = flags.String("dir", ".", "directory with migration files")
+	dir     = flags.String("dir", "", "directory with migration files")
+	table   = flags.String("table", "goose_db_version", "migrations table name")
 	verbose = flags.Bool("v", false, "enable verbose mode")
 	help    = flags.Bool("h", false, "print help")
 	version = flags.Bool("version", false, "print version")
@@ -28,6 +29,7 @@ func main() {
 	if *verbose {
 		goose.SetVerbose(true)
 	}
+	goose.SetTableName(*table)
 
 	args := flags.Args()
 	if len(args) == 0 || *help {

commit 00dcc34f9b4e9dd693f32b3244034aac96642b6c

Fixes #209 (regression from 805f9b21): Add default -dir value

cmd/goose/main.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	flags   = flag.NewFlagSet("goose", flag.ExitOnError)
-	dir     = flags.String("dir", "", "directory with migration files")
+	dir     = flags.String("dir", ".", "directory with migration files")
 	table   = flags.String("table", "goose_db_version", "migrations table name")
 	verbose = flags.Bool("v", false, "enable verbose mode")
 	help    = flags.Bool("h", false, "print help")